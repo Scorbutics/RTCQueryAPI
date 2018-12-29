d6682f615785c0e9f1c3971c05f4b6eb51a1a9cd
Scorbutics <brice.bulgarelli@outlook.com>
2018-12-29 15:17:16 +0100

    Release

---
+++ src/rtcqueryapi/rtcqueryapi.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RtcQueryService } from './querybuilder.service';
+
+@NgModule({
+  imports: [
+    
+  ],
+  exports: [ RtcQueryService ]
+})
+export class RTCQueryAPIModule { }
